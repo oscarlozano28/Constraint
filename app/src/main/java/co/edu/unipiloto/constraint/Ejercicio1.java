3176029755094c6976dbe045ac93ad2ac42e2304
oscarlozano28 <oscar-lozano1@upc.edu.co>
2021-08-23 10:55:48 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/constraint/Ejercicio1.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.constraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ejercicio1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio1);
+    }
+}
---
+++ app/src/main/java/co/edu/unipiloto/constraint/Ejercicio4.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.constraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ejercicio4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio4);
+    }
+}
